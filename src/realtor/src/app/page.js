a1fff79955787096db00305565b8128b5b2be65a
alexjoybc <jonathan.laplante@gmail.com>
2023-04-23 19:37:54 -0700

    wip

--- src/realtor/src/app/page.js
+++ src/realtor/src/app/page.js
@@ -1,11 +1,17 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+    <Head>
+      <title>Next.js TailwindCSS</title>
+      <link rel="icon" href="/favicon.ico"/>
+    </Head>
+
+    <div className="container mx-auto">
+      <h1 className="text-lg text-center m-4">TailwindUI/Next.js</h1>
+      <p className="bg-green-600">This is a test of the tailwind next integration.</p>
+    </div>
+  </div>
   )
 }
