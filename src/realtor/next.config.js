69cda2cc8c6ddeca8c2457d59eb60e8bad45d2f3
alexjoybc <51387119+alexjoybc@users.noreply.github.com>
2023-04-30 09:52:02 -0700

    Update next.config.js

--- src/realtor/next.config.js
+++ src/realtor/next.config.js
@@ -5,13 +5,12 @@ const isGithubActions = process.env.GITHUB_ACTIONS || false
 let assetPrefix = ''
 let basePath = ''
 
-if (isGithubActions) {
+// if (isGithubActions) {
   // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+//  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+//  assetPrefix = `/${repo}/`
+//  basePath = `/${repo}
+//}
 
 const nextConfig = {
   experimental: {
