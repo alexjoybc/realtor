b7838380c36c4e6ffc266cd06937ec122717b11b
alex <alexandre.jx@gmail.com>
2023-04-30 23:47:35 -0700

    remove menu

--- src/realtor/src/app/page.js
+++ src/realtor/src/app/page.js
@@ -241,7 +241,7 @@ export default function Home() {
 
         {/* <span>{JSON.stringify(paymentSchedule)}</span> */}
         <div className="mt-5 mx-2 grid grid-cols-1">
-          <ResponsiveContainer width="100%" height={200}>
+          <ResponsiveContainer width="100%" height={400}>
             <LineChart data={paymentSchedule}>
               <Line yAxisId="right" type="monotone" dataKey="interest" stroke="#8884d8" dot={false} />
               <Line yAxisId="left" type="monotone" dataKey="remaining" stroke="#d1d1d1" dot={false} />
