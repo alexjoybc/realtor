9460a8fe3d6a44a08c12735f30d753c22df1d3f5
alex <alexandre.jx@gmail.com>
2023-05-02 21:27:54 -0700

    fix key issue

--- src/realtor/src/components/dropdown.js
+++ src/realtor/src/components/dropdown.js
@@ -11,7 +11,7 @@ export default function Dropdown({ name, label, options, onChange }) {
                     onChange={onChange}
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                 >
-                    {options.map((option => (<option value={option.value}>{option.label}</option>)))}
+                    {options.map((option => (<option key={option.value} value={option.value}>{option.label}</option>)))}
                 </select>
             </div>
         </div>
